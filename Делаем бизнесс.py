4cedcf2ee46d4824884f0885a81bb636ede8d1f2
Popugayka <101162352+Sux6752@users.noreply.github.com>
2022-10-21 20:19:16 +0700

    Add files via upload

---
+++ Делаем бизнесс.py
@@ -0,0 +1,7 @@
+a=int(input("Сколько потратите от 10  до 10000 "))
+bonusi = 0
+while bonusi<1000:
+    b=a/10
+    bonusi=bonusi+b
+    a=int(input("Сколько потратите от 10  до 10000 "))
+print("время тратить")
