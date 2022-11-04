9dcee935117e7f1279e9bbb0a54e40af13cd7ace
Popugayka <101162352+Sux6752@users.noreply.github.com>
2022-11-04 16:31:36 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,37 @@
+a=0
+for i in range(1,6):
+    print("HHHHHHHH        HHHHHHHH")
+for i in range(1,5):
+    print("HHHHHHHHHHHHHHHHHHHHHHHH")
+for i in range(1,6):
+    print("HHHHHHHH        HHHHHHHH")
+for i in range(1,5):
+    print( )
+for i in range(1,3):
+    for i in range(1,4):
+        print("EEEEEEEEEEEEEEEEEEEEEEEE")
+    for i in range(1,6):
+        print("EEEEEE")
+for i in range(1,4):
+    print("EEEEEEEEEEEEEEEEEEEEEEEE")
+for i in range(1,5):
+    print( )
+for i in range(1,16):
+    print("LLLLLLLL")
+for i in range(1,5):
+    print("LLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+for i in range(1,5):
+    print( )
+for i in range(1,16):
+    print("LLLLLLLL")
+for i in range(1,5):
+    print("LLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+print("              OOOO          ")
+print("            OOOOOOOO        ")
+print("          OOOO    OOOO     ")
+for i in range(1,11):
+    print("        OOOO        OOOO   ")
+print("          OOOO    OOOO     ")
+print("            OOOOOOOO        ")
+print("              OOOO          ")
+    
---
+++ ФАКТОРИАЛ.py
@@ -0,0 +1,6 @@
+sum=1
+a = int(input("Число которое будет в факториале - "))
+for i in range(1, a+1):
+    sum=sum*i
+print('Факториал от 1 до', a, '—', sum)
+    
