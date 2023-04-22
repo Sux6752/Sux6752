52d9b723836f7d40281f02ec35a029a89681b15f
Popugayka <101162352+Sux6752@users.noreply.github.com>
2023-04-22 18:32:08 +0700

    Add files via upload

--- cvx.py
+++ cvx.py
@@ -5,8 +5,8 @@ WIDTH = 1200
 HEIGHT = 600
 FPS = 60
 direction = 0
-x = 200
-y = 100
+x = 550
+y = 550
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -15,7 +15,9 @@ clock = pygame.time.Clock()
 surface = pygame.Surface((125,15))
 color = (255, 255, 255)
 
-while True: 
+while True:
+    pygame.display.flip()
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 150, 20))
     # gets a single event from the event queue 
     event = pygame.event.wait() 
     # if the 'close' button of the window is pressed 
@@ -29,12 +31,14 @@ while True:
         # converts to uppercase the key name 
         key_name = key_name.upper() 
         # if any key is pressed 
-        if event.type == pygame.KEYDOWN: 
+        if event.type == pygame.KEYDOWN:
+            if (event.key == pygame.K_RIGHT):
+                x+=5
             # prints on the console the key pressed 
-            print(u'"{}" key pressed'.format(key_name)) 
+            #print(u'"{}" key pressed'.format(key_name)) 
         # if any key is released 
         elif event.type == pygame.KEYUP: 
             # prints on the console the released key 
             print(u'"{}" key released'.format(key_name))
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
-        pygame.display.flip() 
+        clock.tick(FPS)
+        pygame.display.update()
