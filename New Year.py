97e041761b835d9df9f0df13c13337251a993f6a
Popugayka <101162352+Sux6752@users.noreply.github.com>
2023-09-08 20:33:39 +0700

    Add files via upload

---
+++ New Year.py
@@ -0,0 +1,50 @@
+class Lydi():
+    def __init__(self,bezzubost,gitara,gruppa,harizma):
+        self.bezzubost=bezzubost
+        self.gitara=gitara
+        self.gruppa=gruppa
+        self.harizma=harizma
+        
+    def vfvf(self):
+        a=self.bezzubost+self.gitara+self.gruppa+self.harizma
+        print(a/4)
+print("Горшок :")
+
+
+Gorshok=Lydi(2,10,10,8) #экземпляр класса Люди
+Gorshok.vfvf()
+
+
+print(Gorshok.bezzubost,"/ 32")
+print(Gorshok.gitara,"/ 10")
+print(Gorshok.gruppa,"/ 10")
+print(Gorshok.harizma,"/ 10")
+
+print("")
+
+print("Цой:")
+Tsoi=Lydi(0,10,10,10)
+Tsoi.vfvf()
+print(Tsoi.bezzubost,"/ 32")
+print(Tsoi.gitara,"/ 10")
+print(Tsoi.gruppa,"/ 10")
+print(Tsoi.harizma,"/ 10")
+
+print("")
+
+print("Гигачад:")
+Giga=Lydi(0,0,0,999999)
+Giga.vfvf()#
+print(Giga.bezzubost,"/ 32")
+print(Giga.gitara,"/ 10")
+print(Giga.gruppa,"/ 10")
+print(Giga.harizma,"/ 10")
+
+
+
+
+# class rectangle():
+#     def rect(y,y,r,t):
+#         gvyvgvgvygv
+#         
+# size.rect()
