5af0e6622b5eb6a8f2df365ba498cbac2ab01b25
Popugayka <101162352+Sux6752@users.noreply.github.com>
2023-11-10 20:32:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import telebot
-#from telebot import types
+from telebot import types
 bot=telebot.TeleBot("6980649881:AAFVwmGatEDeqK6sYfe1h_CSNHXisertdj0")
 
-@bot.message_handler(commands=['start'])
-def start(message, res = False):
-    # a = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # q =types.KeyboardButton("Test#1")
-    # k =types.KeyboardButton("Test#2")
-    # a.add("Тест#1")
-    bot.send_message(message.chat.id, "Тест 1 - сделан как и тест 2 в 19:30", "У меня есть кошки", message.text)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    if message.text.lower() == 'хай':
+        bot.send_message(message.chat.id,'<b>TortI</b>', parse_mode='html')
+    elif message.text.lower() == 'qbjk':
+        bot.send_message(message.chat.id,'28 Ударов ножом!')
+    elif message.text.lower() == 'Весна':
+        bot.send_message(message.chat.id,'Коты прилетели')
+
 
     bot.send_message(message.chat.id,"Test",message.text)
+@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
+def star(message):
+        # a = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    a = types.ReplyKeyboardMarkup()
+
+    button1 = types.KeyboardButton("Test#1")
+        # button1 =types.KeyboardButton("Test#1")
+
+    button2 = types.KeyboardButton("Test#2")
+        # button2 =types.KeyboardButton("Test#2")
+
+    button3 = types.KeyboardButton("Test#3")
+
+    a.row(button1)
+    bot.send_message(message.chat.id,  reply_markup=a)
 if __name__== '__main__':
     bot.polling(none_stop = True, interval=0)
-
