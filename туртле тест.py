8c42031fc959b29d95f9390b76a483fba749dbec
Popugayka <101162352+Sux6752@users.noreply.github.com>
2024-03-30 20:31:06 +0700

    Add files via upload

---
+++ туртле тест.py
@@ -0,0 +1,27 @@
+import turtle as turt
+turt.pensize(1)
+turt.pencolor('blue')
+turt.speed(5000)
+while True:
+    turt.forward(100)
+    turt.left(135)
+    turt.forward(50)
+    turt.right(90)
+#     turt.forward(50)
+    turt.left(135)
+    turt.forward(100)
+#    turt.left(90)
+    turt.forward(70)
+#    turt.forward(150)
+    turt.left(135)
+    turt.forward(100)
+    turt.left(90)
+    turt.forward(100)
+    turt.left(10)
+    turt.left(0.2)
+    turt.forward(0.1)
+
+    
+
+
+
